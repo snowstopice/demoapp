5ae9fc2730d2167d59daaa743d7355b81890497c
sonwstopie <kristy.zhou.sysu@gmail.com>
2015-02-27 03:01:43 +0000

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  match 'help', to: 'static_pages#help', via: 'get'
+  match 'help', to: 'static_pages#ghelp', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
